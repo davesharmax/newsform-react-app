cd196a5375175dac1f9e1553557143a42c5cef13
davesharmax <97398369+davesharmax@users.noreply.github.com>
2023-07-03 23:34:34 +0530

    Getting started

---
+++ src/Components/News.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import NewsTtem from './NewsTtem'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        this is a news componenet
+        <NewsTtem/>
+      </div>
+    )
+  }
+}
+
+export default News
---
+++ src/Components/NewsTtem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsTtem extends Component {
+  render() {
+    return (
+      <div>
+        this is a newsitem  componenet
+      </div>
+    )
+  }
+}
+
+export default NewsTtem
