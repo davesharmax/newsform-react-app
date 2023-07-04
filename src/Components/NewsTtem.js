479bb65f6028afce4962032e0d9f4b63496a687a
davesharmax <97398369+davesharmax@users.noreply.github.com>
2023-07-04 15:46:31 +0530

    news_showing

--- src/Components/NewsTtem.js
+++ src/Components/NewsTtem.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsTtem extends Component {
+
   render() {
+    let { title, description,imgUrl,newsUrl } = this.props;
     return (
-      <div>
-        this is a newsitem  componenet
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={imgUrl} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}...</p>
+          <a href={newsUrl} target ="_blank" className="btn byn-sm btn-primary">
+            Read more
+          </a>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsTtem
+export default NewsTtem;
