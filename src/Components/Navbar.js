6905cd254eedb9331cab7193e64804d6abe96b9b
davesharmax <97398369+davesharmax@users.noreply.github.com>
2023-07-05 23:55:02 +0530

    minor_changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -21,14 +21,18 @@ export class Navbar extends Component {
           <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">Link</a>
+          <a className="nav-link" href="/">sports</a>
         </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-         
+        <li className="nav-item">
+          <a className="nav-link" href="/">health</a>
+        </li>      
+          <li className="nav-item">
+          <a className="nav-link" href="/">technology</a>
         </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">science</a>
+        </li>
+
 
       </ul>
       <form className="d-flex" role="search">
--- src/Components/NewsTtem.js
+++ src/Components/NewsTtem.js
@@ -4,13 +4,14 @@ export class NewsTtem extends Component {
   render() {
     let { title, description, imgUrl, newsUrl } = this.props;
     return (
-      <div className="card" style={{ width: "18rem" }}>
+      <div className="card" >
         <img src={imgUrl} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{description}...</p>
           <a
             href={newsUrl}
+            rel="noreferrer"
             target="_blank"
             className="btn byn-sm btn-dark btn-primary"
           >
